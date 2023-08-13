7a6a16a79e99d76a01d97bd06b23ded224bc428d
Michila <michila.bimsara12@gmail.com>
2023-08-14 01:33:31 +0530

    set word to guess

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react"
+import words from "./wordList.json"
 
 
 function App() {
-  
+  const [wordToGuess, setWordToGuess] = useState(() => (
+    return (words[Math.floor(Math.random()* words.length)]
+  );
 
+  console.log(wordToGuess)
   return (
     <h1>hello world</h1>
   )
+  
 }
 
 export default App
