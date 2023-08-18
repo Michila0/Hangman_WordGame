9da6f259b484ef4c22148d5e064742d14d583ce5
Michila <michila.bimsara12@gmail.com>
2023-08-18 21:53:42 +0530

    add the logic case but has the error

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,67 @@
-import { useState } from "react"
-import words from "./wordList.json"
+import { useEffect, useState } from "react"
 import { HangmanDrawing } from "./HangmanDrawing"
 import { HangmanWord } from "./HangmanWord"
 import { Keyboard } from "./Keyboard"
+import words from "./wordList.json"
 
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
-    
     return words[Math.floor(Math.random()* words.length)]
   })
-  const [guessedLetters, setGuessedLetters] = useState<String[]>(["g"])
+  const [guessedLetters, setGuessedLetters] = useState<String[]>([])
   //console.log(wordToGuess)
 
   const incorrectLetters = guessedLetters.filter(
-    lettter => !wordToGuess.includes(lettter)
+    letter => !wordToGuess.includes(letter)
   )
-  return <div style={{
-    maxWidth: "800px",
-    display: "flex",
-    flexDirection: "column",
-    gap: "2rem",
-    margin: "0 auto",
-    alignItems: "center"
-  }}>
-    <div style={{fontSize: "2rem", textAlign: "center"}}>
-      Lose
-      Win
-    </div>
-    <HangmanDrawing numberOfGuesses = {incorrectLetters.length} />
-    <HangmanWord/>
-    <div style={{ alignSelf: "stretch"}}>
-      <Keyboard/>
-    </div>
+
+  function addGuessedLetter(letter: string) {
+    if (guessedLetters.includes(letter)) return
+
+    setGuessedLetters(currentLetters => [...currentLetters, letter])
+  }
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+
+      if(!key.match(/^[a-z]$/)) return
+      
+      e.preventDefault()
+      addGuessedLetter(key)
+    }
+
+    document.addEventListener("keypress", handler)
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [])
+  return (
+    <div 
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center"
+      }}
+    >
+      <div style={{fontSize: "2rem", textAlign: "center"}}>
+        Lose
+        Win
+      </div>
+      <HangmanDrawing numberOfGuesses = {incorrectLetters.length} />
+      
+      <HangmanWord guessedLetters = {guessedLetters} wordToGuess = {wordToGuess}/>
+
+      <div style={{ alignSelf: "stretch"}}>
+        <Keyboard/>
+      </div>
     
-  </div>
+    </div>
+  )
   
   
 }
